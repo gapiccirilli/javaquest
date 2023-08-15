81f7b3fd27caaa163715679bbf9ab37d5e142ef1
Gino Piccirilli <gapiccirilli@outlook.com>
2023-08-15 11:35:05 -0400

    render leaflet map

--- src/components/sections/Map.js
+++ src/components/sections/Map.js
@@ -1,10 +1,24 @@
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
 
 export default function Home() {
-  
-    return (
-      <section className={`${styles.map} sections`}>
+    const [position, setPosition] = useState([47.36930, 8.54181]);
 
+    return (
+      <section className={`${styles.mapContainer} sections`}>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={true} className={styles.map}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>
+            contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url='https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png'
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
       </section>
     );
   }
